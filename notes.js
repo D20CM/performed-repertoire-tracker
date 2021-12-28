fef1dce350092026727fe67956a923ccad0cb7ce
D20CM <scottbrown500@hotmail.com>
2021-12-28 14:59:00 +0000

    initial commit

---
+++ notes.js
@@ -0,0 +1,4 @@
+//I'd like to build a system that can keep track of which songs or pieces of music have been performed in a given period.
+//This would be preety useful when performing as a musician in a venue over multiple nights - for example a cruise ship or hotel residency where you might be entertaining the same audience every night for a week or more.  Having a large repertoire of material to work with is obviously very important in this situation, but it can sometimes be difficult to keep track of what songs or pieces have been performed - since the start of the week, for example.
+// It would also be a benefit to flag up songs or pieces that haven't been performed in a while.
+//
